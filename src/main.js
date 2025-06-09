e7a16530ba76999a3101314ef9dbf379dbec1ae1
WeddingBulatGuzel <ya@lgaffatullina.ru>
2025-06-09 17:15:22 +0500

    clean

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import VueEasyLightbox from 'vue-easy-lightbox';
-import '../style.css'
-
-const app = createApp(App);
-app.use(VueEasyLightbox);
-app.mount('#app'); 
